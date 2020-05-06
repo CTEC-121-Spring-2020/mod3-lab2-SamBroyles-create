31ef708e84ed93a69a7879640b83816281ab35c0
Sam Broyles <s.broyles@students.clark.edu>
2020-05-06 12:13:12 -0700

    Mod 3 Lab 2 completed

---
+++ ExceptionDemo.py
@@ -0,0 +1,23 @@
+"""
+CTEC 121
+<your name>
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    try:
+        print(4/0)
+    except ZeroDivisionError:
+        print("\nThere was a divide by zero error. Exiting\n")
+    except:
+        print("\nUnknown Exception\n") 
+        exit
+
+main()    
